78008b48a52b47f4160bc236f9e3679b23c3c76e
AhmedSelim04 <ahmed.selim.fci@gmail.com>
2025-08-19 00:15:41 +0300

    add utility & Models folders

---
+++ Bulky.Models/AppUser.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BulkyBook.Models
+{
+    public class AppUser: IdentityUser
+    {
+        [Required]
+        public string? Name { get; set; }
+        public string? StreetAddress { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public string? PostalCode { get; set; }
+        [ForeignKey("Company")]
+        [ValidateNever]
+        public int? CompanyId { get; set; }
+        public Company Company { get; set; }
+    }
+}
---
+++ Bulky.Models/Category.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBook.Models
+{
+    public class Category
+    {
+        // if primary key is not like that (Id - <ClassName>Id) 
+        // then use [Key] attribute to specify primary key (Data annotation -> attribute)
+
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Name is required.")]
+        [DisplayName("Category Name")]
+        [MaxLength(30)]
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1,100)]
+        public int DisplayOrder { get; set; } = 0;
+        public string ImageUrl { get; set; } = string.Empty;
+        
+
+    }
+}
